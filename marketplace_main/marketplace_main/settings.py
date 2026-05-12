93c577205a63d4293840afc926e1864841b1319f
Jocelyn12344444 <joselynmartinezurcino@gmail.com>
2026-05-11 18:05:11 -0700

    actualizacion

--- marketplace_main/marketplace_main/settings.py
+++ marketplace_main/marketplace_main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ea&a#kpfzz8w0=r0_@5y9bp3g%$k*p4$12o^u&5vf#%zr*js+('
+SECRET_KEY = 'django-insecure--xj9_ozda2hstc0ubg-6=20@cr%fr=yug#dkrriu82))1+4gp3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+     'store',
 ]
 
+AUTH_USER_MODEL = 'store.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
